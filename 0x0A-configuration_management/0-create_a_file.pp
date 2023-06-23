bfe6dd07179cd09921f511c12cffbd9146ecd8cb
Juliusmwash <cutejuliusmwash@gmail.com>
2023-06-23 22:29:14 +0300

    Solve task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Creating a file containing 'I love Puppet'
+
+file { '/tmp/school':
+  ensure  => 'file',
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
+
