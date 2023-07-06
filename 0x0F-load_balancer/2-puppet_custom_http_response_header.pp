235f31e6b21d893a240a28d9b17fd9405e6ff29f
Juliusmwash <cutejuliusmwash@gmail.com>
2023-07-06 04:07:27 +0300

    Configure loadbalancer using puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,23 +1,7 @@
-# Define a class for custom HTTP header configuration
-class custom_http_response_header {
-
-  # Define a fact to retrieve the hostname of the server
-  $hostname = $facts['networking']['hostname']
-
-  # Configure Nginx to include the custom HTTP header
-  file { '/etc/nginx/conf.d/custom_headers.conf':
-    ensure  => file,
-    content => "add_header X-Served-By $hostname;"
-  }
-
-  # Notify the Nginx service to reload the configuration
-  service { 'nginx':
-    ensure  => running,
-    require => File['/etc/nginx/conf.d/custom_headers.conf'],
-    notify  => Service['nginx'],
-  }
+# Configure loadbalancer using puppet
+exec { 'configure_loadbalancer':
+  provider => shell,
+  command => 'git clone https://github.com/Juliusmwash/alx-system_engineering-devops.git ; mv alx-system_engineering-devops/0x0F-load_balancer/0-custom_http_response_header . ; rm -rf alx-system_engineering-devops ; sudo ./0-custom_http_response_header',
+  path => '/usr/bin:/bin',
 }
-
-# Include the custom HTTP header configuration class
-include custom_http_response_header
 
