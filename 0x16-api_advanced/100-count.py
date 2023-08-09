a6c0dc1d2004ee56e1d0e9cce5aa65745e8d5883
Juliusmwash <cutejuliusmwash@gmail.com>
2023-08-09 03:52:59 +0300

    Restructure 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -23,7 +23,7 @@ def count_words(subreddit, word_list, after=None, counts=None):
         url = f"https://www.reddit.com/r/{subreddit}/hot.json?after={after}"
 
     headers = {"User-Agent": "Reddit Keyword Counter"}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
