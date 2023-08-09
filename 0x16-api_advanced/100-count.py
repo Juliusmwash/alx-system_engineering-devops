2c64728a6aee984c07ba58d2ae8e8f1765e3b152
Juliusmwash <cutejuliusmwash@gmail.com>
2023-08-09 03:35:11 +0300

    Restructure 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -46,5 +46,5 @@ def count_words(subreddit, word_list, after=None, counts=None):
             sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
             for word, count in sorted_counts:
                 print(f"{word}: {count}")
-    else:
-        print(f"Error: Unable to fetch data from subreddit '{subreddit}'")
+#    else:
+#        print(f"Error: Unable to fetch data from subreddit '{subreddit}'")
