cff1996916e2a91d1ebce192ed92b2a21eff0e3d
Juliusmwash <cutejuliusmwash@gmail.com>
2023-08-20 03:14:14 +0300

    Solve puppent-lint error

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,17 +1,13 @@
-# Increases the amount of traffic an Nginx server can handle.
+#Increases the amount of traffic an Nginx server can handle.
 
 # Increase the ULIMIT of the default file
-exec { 'fix_for_nginx':
-  command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin:/bin',
-  onlyif  => '/bin/grep -q "ULIMIT=4096" /etc/default/nginx',
-  notify  => Exec['nginx_restart'],
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
 }
 
 # Restart Nginx
-exec { 'nginx_restart':
-  command     => '/etc/init.d/nginx restart',
-  path        => ['/usr/local/sbin', '/usr/local/bin', '/sbin', '/bin', '/usr/sbin', '/usr/bin'],
-  refreshonly => true,
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
-
